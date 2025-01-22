36d5d4a89aa89945c783308b74141d973e5f4dab
AlgoClassWork <kyxec.play@gmail.com>
2025-01-22 17:16:18 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #pip install flask
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from database import *
 
 app = Flask(__name__)
@@ -8,5 +8,18 @@ app = Flask(__name__)
 def index():
     organs = get_all_organs()
     return render_template('index.html', organs=organs)
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        category = request.form.get('category')
+        description = request.form.get('description')
+        price = request.form.get('price')
+        image = request.form.get('image')
+
+        add_organ(name, category, description, price, image)
+
+    return render_template('admin.html')
 
 app.run()
